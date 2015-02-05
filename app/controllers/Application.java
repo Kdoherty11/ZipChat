1d4cad63b08a7ad241bee68c2950949aa03801cc
Zachary Webert <zachary@attend.com>
2015-02-04 22:43:28 -0500

    added rooms endpoint

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,6 +2,10 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import models.Room;
+import play.db.ebean.Model;
+import java.util.List;
+import static play.libs.Json.toJson;
 
 import views.html.*;
 
@@ -9,6 +13,18 @@ public class Application extends Controller {
 
     public static Result index() {
         return ok(index.render("Your new application is ready."));
+    }
+
+    public static Result createRoom() {
+        Room room = new Room();
+        room.name = "Test Room";
+        room.save();
+        return ok();
+    }
+
+    public static Result getRooms() {
+        List<Room> tasks = new Model.Finder(String.class, Room.class).all();
+        return ok(toJson(tasks));
     }
 
 }
---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,26 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table room (
+  id                        varchar(255) not null,
+  name                      varchar(255),
+  constraint pk_room primary key (id))
+;
+
+create sequence room_seq;
+
+
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists room;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists room_seq;
+
