2aea180f8c961992f7ee0468653e60b387df2ad4
Zachary Webert <zachary@attend.com>
2015-03-04 21:20:49 -0500

    .

--- app/models/entities/AbstractRoom.java
+++ app/models/entities/AbstractRoom.java
@@ -8,11 +8,8 @@ import javax.persistence.*;
 public class AbstractRoom {
 
     @Id
-    @SequenceGenerator(name="room_id_seq",
-            sequenceName="room_id_seq",
-            allocationSize=1)
-    @GeneratedValue(strategy = GenerationType.IDENTITY,
-            generator="room_id_seq")
+    @GeneratedValue(strategy = GenerationType.TABLE)
+
     public long roomId;
 
 }
