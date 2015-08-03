7d271ce06bd976f8c319ba6ab64531f339ba046f
Kevin Doherty <kdoherty11@yahoo.com>
2015-08-03 15:58:27 -0400

    Fixed tests

---
+++ test/unit/daos/AbstractDaoTest.java
@@ -0,0 +1,23 @@
+package unit.daos;
+
+import com.google.common.collect.ImmutableMap;
+import play.Application;
+import play.test.WithApplication;
+
+import static play.test.Helpers.fakeApplication;
+
+/**
+ * Created by kdoherty on 7/31/15.
+ */
+class AbstractDaoTest extends WithApplication {
+
+    @Override
+    protected Application provideApplication() {
+        return fakeApplication(ImmutableMap.of(
+                "db.default.driver", "org.h2.Driver",
+                "db.default.url", "jdbc:h2:mem:play-test-jpa",
+                "db.default.jndiName", "DefaultDS",
+                "jpa.default", "defaultPersistenceUnit"
+        ));
+    }
+}
